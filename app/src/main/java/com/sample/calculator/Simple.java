b9851ac0222dab6ce3cb930371b2fd090bd63965
Bartłomiej Wysocki <bartekwysocki46@gmail.com>
2018-03-14 14:40:06 +0100

    working simple operations with error detections layout portrait and landscape done

--- app/src/main/java/com/sample/calculator/Simple.java
+++ app/src/main/java/com/sample/calculator/Simple.java
@@ -1,10 +1,12 @@
 package com.sample.calculator;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by Bartłomiej on 2018-03-12.
@@ -27,6 +29,7 @@ public class Simple extends AppCompatActivity {
     String firstNumber;
     String secondNumber;
     boolean operationFlag = false;
+    boolean isMinus = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,6 +44,7 @@ public class Simple extends AppCompatActivity {
         seven = (Button)findViewById(R.id.seven);
         eight = (Button)findViewById(R.id.eight);
         nine = (Button)findViewById(R.id.nine);
+        coma = (Button)findViewById(R.id.coma);
         textView.setText("");
     }
 
@@ -55,10 +59,36 @@ public class Simple extends AppCompatActivity {
     }
 
     public void clear(View view){
+        coma.setEnabled(true);
         textView.setText("");
+        firstNumber = "";
+        secondNumber = "";
+    }
+
+    public void symbolChange(View view){
+        if(isMinus == false) {
+            textView.setText("-" + textView.getText());
+            isMinus = true;
+        }else if(isMinus == true) {
+            String text = textView.getText().toString();
+            if(text != "" && text !=null && text.length() > 0 || text.substring(0,0) == "-"){
+                text = text.substring(1,text.length());
+                textView.setText(text);
+
+            }
+            isMinus = false;
+        }
+    }
+
+    public void addComa(View view){
+        if(textView.getText() != ""){
+            textView.setText(textView.getText()+".");
+        }
+        coma.setEnabled(false);
     }
 
     public void sum(View view){
+        coma.setEnabled(true);
         operationFlag = true;
         currentOperation = "sum";
         firstNumber = textView.getText().toString();
@@ -67,9 +97,60 @@ public class Simple extends AppCompatActivity {
     }
 
     public void equal(View view){
+        double equal = 0;
         secondNumber=textView.getText().toString();
-        int equal = Integer.parseInt(firstNumber)+Integer.parseInt(secondNumber);
-        textView.setText(Integer.toString(equal));
+        if(currentOperation=="sum") {
+            equal = Double.parseDouble(firstNumber) + Double.parseDouble(secondNumber);
+            coma.setEnabled(false);
+            textView.setText(Double.toString(equal));
+        }else if(currentOperation=="multiplication") {
+            equal = Double.parseDouble(firstNumber) * Double.parseDouble(secondNumber);
+            coma.setEnabled(false);
+            textView.setText(Double.toString(equal));
+        }else if(currentOperation=="subtraction"){
+            equal = Double.parseDouble(firstNumber) - Double.parseDouble(secondNumber);
+            coma.setEnabled(false);
+            textView.setText(Double.toString(equal));
+        }else if(currentOperation=="division"){
+            if(Double.parseDouble(secondNumber) == 0) {
+                Context context = getApplicationContext();
+                CharSequence text = "You can't divide by zero!";
+                int duration = Toast.LENGTH_SHORT;
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }else {
+                equal = Double.parseDouble(firstNumber) / Double.parseDouble(secondNumber);
+                coma.setEnabled(false);
+                textView.setText(Double.toString(equal));
+            }
+        }
+
+
+    }
+
+
+    public void division(View view){
+        coma.setEnabled(true);
+        operationFlag = true;
+        currentOperation="division";
+        firstNumber = textView.getText().toString();
+        textView.setText("");
+    }
+
+    public void multiplication(View view){
+        coma.setEnabled(true);
+        operationFlag = true;
+        currentOperation = "multiplication";
+        firstNumber = textView.getText().toString();
+        textView.setText("");
+    }
+
+    public void subtraction(View view){
+        coma.setEnabled(true);
+        operationFlag = true;
+        currentOperation="subtraction";
+        firstNumber = textView.getText().toString();
+        textView.setText("");
     }
 
     public void addNumber_0(View view){
